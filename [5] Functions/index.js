613a73b9cfa5ad8e3ea01c3260b5feaafd4b0da8
HellEaglee <helleagleee@gmail.com>
2024-06-24 11:40:19 +0300

    Functions

---
+++ [5] Functions/index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+function bubblesort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let tmp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = tmp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubblesort([4, 2, 3, 6, 1, 2]));
